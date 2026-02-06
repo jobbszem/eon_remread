b93666aacc2ef135be74dc8e10403fa6bbca4f82
Tóth Péter <jobbszem@gmail.com>
2026-02-06 19:51:47 +0100

    async_forward_entry_setups

--- custom_components/eon_remread/__init__.py
+++ custom_components/eon_remread/__init__.py
@@ -27,9 +27,7 @@ async def async_setup_entry(hass, entry):
     if success:
         hass.data[DOMAIN][entry.entry_id] = api
 
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, "sensor")
-        )
+        await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
         return True
     else:
